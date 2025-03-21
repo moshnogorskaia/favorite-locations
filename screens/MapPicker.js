c524ae39048bc76453e9e4b6ecfd94e50cd3e877
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-03-21 11:16:32 +0100

    Add a map marker

--- screens/MapPicker.js
+++ screens/MapPicker.js
@@ -1,7 +1,10 @@
 import MapView, { Marker } from "react-native-maps";
 import { StyleSheet, View } from "react-native";
+import { useState } from "react";
 
 function MapPicker() {
+	const [selectedLocation, setSelectedLocation] = useState();
+
 	const region = {
 		latitude: 37.78,
 		longitude: -122.43,
@@ -9,7 +12,30 @@ function MapPicker() {
 		longitudeDelta: 0.042,
 	};
 
-	return <MapView initialRegion={region} style={styles.map} />;
+	function selectLocationHandler(event) {
+		const lat = event.nativeEvent.coordinate.latitude;
+		const lng = event.nativeEvent.coordinate.longitude;
+
+		setSelectedLocation({ lat, lng });
+	}
+
+	return (
+		<MapView
+			initialRegion={region}
+			style={styles.map}
+			onPress={selectLocationHandler}
+		>
+			{selectedLocation && (
+				<Marker
+					title="Picked Location"
+					coordinate={{
+						latitude: selectedLocation.lat,
+						longitude: selectedLocation.lng,
+					}}
+				/>
+			)}
+		</MapView>
+	);
 }
 
 export default MapPicker;
